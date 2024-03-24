a686c153cd62716a05ff6417bd7cba2ceb79cb3f
kozdavaa <davidkozmark@gmail.com>
2024-03-24 20:45:05 +0100

    start demodulation code

--- meres/adsb.c
+++ meres/adsb.c
@@ -9,7 +9,7 @@
 #define FIR_LEN  8
 
 // input data buffer length
-#define BUF_SIZE  8192
+#define BUF_SIZE  131072
 
 int main(int argc,char **argv)
 {
@@ -41,9 +41,9 @@ int main(int argc,char **argv)
 	// variables
 	unsigned int abs_val;
 	int accumulator = 0;
-	unsigned char bit;
-	unsigned int stm;
-	unsigned char hex;
+	unsigned char bit=0;
+	unsigned int stm=0;
+	unsigned char hex=0;
 
 	i = 0;
 	j = 0;
@@ -52,6 +52,7 @@ int main(int argc,char **argv)
 	do {
 		read_len = fread(buffer, 1, BUF_SIZE, stdin);	// read data to input buffer
 
+
 		for(bix=0; bix<read_len; bix+=2) {
 			// convert I-Q to magnitude
 			abs_val=iq_to_abs[buffer[bix]][buffer[bix +1]];
@@ -59,13 +60,45 @@ int main(int argc,char **argv)
 			accumulator -= fifo[fptr];
 			accumulator += abs_val;
 			fifo[fptr] = abs_val;
+			i =  (FIR_LEN - (fptr-(FIR_LEN/2)))%FIR_LEN; //pointer for the abs_val
 			fptr = fptr+1;
 			fptr = fptr%FIR_LEN;
 			// Decoding
-
+			if (fifo[i] > (accumulator/FIR_LEN))
+				bit = 1;
+			else
+				bit = 0;
 			// ADS-B packet search and print
-
-			//printf( "%d\t%d\t%d\t%d\n", buffer[bix], buffer[bix+1], abs_val, accumulator/FIR_LEN);
+			if (stm < 16)
+			{
+				if(bit == adsb_preamble[stm])
+					stm++;
+				else
+					stm = 0;
+			}
+			else
+			{
+				if (stm==16) printf("*");
+				if((stm>=16)&&(stm<=PCKT_LEN)&&((stm%2)==0))
+				{
+					if(bit==1)hex=hex|1;
+					hex = hex<<1;
+					j++;
+					if(j==7) {
+						printf("%02x",hex);
+						hex = 0;
+						j = 0;
+					}
+					stm++;
+				}
+				else if((stm>=16)&&(stm<=PCKT_LEN)) stm++;
+				else
+				{
+					printf(";\r\n");
+					stm = 0;
+				}
+			}
+			//printf( "%d\t%d\t%d\t%d\t%d\t%d\t%d\n", buffer[bix], buffer[bix+1], abs_val, accumulator/FIR_LEN, bit,stm,hex);
 		}
 
 		// uncomment if not testing
